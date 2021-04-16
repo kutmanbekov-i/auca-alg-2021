7cf17a7ac52ba246a6ccc6616cdab4dd194d06ac
Kutmanbekov Iskender <kutmanbekov_i@auca.kg>
2021-04-16 23:01:40 +0600

    UVA: 939 – accEPteD

--- project01/uva939/main.cpp
+++ project01/uva939/main.cpp
@@ -2,59 +2,78 @@
 
 using namespace std;
 
+void determine_genes(const string &child, const string &parent1, const string &parent2, map<string,string> &genes, map< string, vector<string> > &parents)
+{
+    if (genes.find(parent1) == genes.end())
+    {
+        determine_genes(parent1, parents[parent1][0], parents[parent1][1], genes, parents);
+    }
+
+    if (genes.find(parent2) == genes.end())
+    {
+        determine_genes(parent2, parents[parent2][0], parents[parent2][1], genes, parents);
+    }
+
+    bool has_gene = false;
+
+    if (genes[parent1] != "non-existent" and genes[parent2] != "non-existent")
+    {
+        has_gene = true;
+    }  else if (genes[parent1] == "dominant" or genes[parent2] == "dominant")
+    {
+        has_gene = true;
+    }
+
+    if (has_gene)
+    {
+        if (genes[parent1] == "dominant" and genes[parent2] == "dominant")
+        {
+            genes[child] = "dominant";
+        } else if (genes[parent1] == "dominant" and genes[parent2] == "recessive")
+        {
+            genes[child] = "dominant";
+        } else if (genes[parent1] == "recessive" and genes[parent2] == "dominant")
+        {
+            genes[child] = "dominant";
+        } else {
+            genes[child] = "recessive";
+        }
+    } else {
+        genes[child] = "non-existent";
+    }
+}
+
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
     
+    string name, name_or_gene;
     int n; cin >> n;
-    string name, second;
     
-    map <string, pair<string, string>> children;
-    map <string, string> genes;
-    
-    for (int i = 0; i < n; ++i)
+    map<string,string> genes;
+    map< string, vector<string> > parents;
+
+
+    while (n--)
     {
-        cin >> name >> second;
-        if (second == "dominant" or second == "recessive" or second == "non-existent")
-                genes[name] = second;
-        else
+        cin >> name >> name_or_gene;
+
+        if (name_or_gene == "non-existent" or name_or_gene == "recessive" or name_or_gene == "dominant")
         {
-                if (children[second].first == "")
-                    children[second].first = name;
-                else
-                    children[second].second = name;
-                    
-                // genes[second]
+            genes[name] = name_or_gene;
+        } else
+        {
+            parents[name_or_gene].push_back(name);
         }
     }
-    
-    for (int i = 0; i < children.size(); ++i)
-        for (auto it = children.begin(); it != children.end(); ++it)
-        {
-            if (genes[it->second.first] == "non-existent" and genes[it->second.second] == "non-existent" or
-                genes[it->second.first] == "recessive" and genes[it->second.second] == "non-existent" or
-                genes[it->second.first] == "non-existent" and genes[it->second.second] == "recessive")
-            {
-                genes[it->first] = "non-existent";
-            }
-            if (genes[it->second.first] == "dominant" and genes[it->second.second] == "non-existent" or
-                     genes[it->second.first] == "non-existent" and genes[it->second.second] == "dominant" or
-                     genes[it->second.first] == "recessive" and genes[it->second.second] == "recessive")
-            {
-                genes[it->first] = "recessive";
-            }
-            if (genes[it->second.first] == "dominant" and genes[it->second.second] == "dominant" or
-                genes[it->second.first] == "dominant" and genes[it->second.second] == "recessive" or
-                genes[it->second.first] == "recessive" and genes[it->second.second] == "dominant")
-            {
-                genes[it->first] = "dominant";
-            }
-            // cout << it->first << ": " << genes[it->second.first] << " " << genes[it->second.second]<< '\n';
-        }
-    
-    
-    for (auto it = genes.begin(); it != genes.end(); ++it)
+
+    for (auto it = parents.begin(); it != parents.end(); it++)
+    {
+        determine_genes(it->first, (it->second)[0], (it->second)[1], genes, parents);
+    }
+
+    for (auto it = genes.begin(); it != genes.end(); it++)
     {
         cout << it->first << " " << it->second << '\n';
     }
