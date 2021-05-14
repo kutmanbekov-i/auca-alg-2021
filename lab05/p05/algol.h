4c97e0637479419b01520c096be6cb4972305d50
Kutmanbekov Iskender <kutmanbekov_i@auca.kg>
2021-05-14 16:24:42 +0600

    modified Count sort

--- lab05/p05/algol.h
+++ lab05/p05/algol.h
@@ -10,17 +10,22 @@ void count_sort(std::vector<int>& v)
  
     std::vector<int> count(range), output(v.size());
     
+    // Store the count of each element (start with index 0)
     for (int i = 0; i < (int)v.size(); i++)
         count[v[i] - min]++;
  
+    // Store the cummulative count of each array
     for (int i = 1; i < (int)count.size(); i++)
         count[i] += count[i - 1];
- 
+
+    // Find the index of each element of the original array in count array, and
+    // place the elements in output array
     for (int i = (int)v.size() - 1; i >= 0; i--) {
         output[count[v[i] - min] - 1] = v[i];
         count[v[i] - min]--;
     }
  
+    // Copy the sorted elements into original array
     for (int i = 0; i < (int)v.size(); i++)
         v[i] = output[i];
 }
