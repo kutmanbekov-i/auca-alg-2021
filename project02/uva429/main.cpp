207f998b5f09d103735a3c6df261355931014a06
Kutmanbekov Iskender <kutmanbekov_i@auca.kg>
2021-05-23 22:56:46 +0600

    UVA: 429 – wronG answeR

---
+++ project02/uva429/main.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+static vector <int> dx = {1, 0, -1, 0, 1, 1, -1, -1};
+static vector <int> dy = {0, 1, 0, -1, -1, 1, 1, -1};
+
+int main()
+{
+    ios_base::sync_with_stdio(0);   cin.tie(NULL);
+    
+    int t; cin >> t;
+    string word, line;
+    
+    while (t--)
+    {
+        cin.ignore();
+        
+        vector<string> words;
+        
+        
+        while (cin >> word and word != "*")
+        {
+            words.emplace_back(word);
+        }
+        
+        string start, end;
+        
+        while (cin >> start >> end and start != "")
+        {
+            // istringstream sinp(line);
+            
+            // string start, end;
+            // sinp 
+            // BFS
+            queue<string> q;
+            map<string, int> transform;
+
+            transform[start] = 0;
+            q.push(start);
+            
+            while (!q.empty() and transform.count(end) == 0)
+            {
+                string u = q.front();
+                q.pop();
+                // Loop over every word in the dictionary.
+                for (size_t i = 0; i < words.size(); ++i)
+                {
+                    const string &v = words[i];
+                    if (! transform.count(v) && u.length() == v.length())
+                    {
+                        int diff = 0;
+                        for (size_t j = 0; j < u.length(); ++j)
+                            if (u[j] != v[j])
+                                ++diff;
+                        if (diff == 1)
+                        {
+                            transform[v] = transform[u] + 1;
+                            q.push(v);
+                        }
+                    }
+                }
+            }    
+            cout << start << " " << end << " " << transform[end] << '\n';
+        }
+        if (t)
+                cout << '\n';
+    }
+    
+    return 0;
+}
